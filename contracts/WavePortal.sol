ebbb12662d87d1f559a61a3b3427b29dce27713f
Guilherme Garros <62261737+GuiGarros@users.noreply.github.com>
2023-03-25 10:33:45 -0300

    criação das estruturas iniciais

---
+++ contracts/WavePortal.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract WavePortal {
+    constructor() {
+        console.log(
+            "O primeiro de muitos contratos que vao revolucionar vidas"
+        );
+    }
+}
